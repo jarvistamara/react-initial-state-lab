356dc106194f1d6d8985a160e92d01640ad3fa5d
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-15 22:18:11 -0400

    commit tree 83d3ff6c3b86bc8d4921270d3461f23aac3e45e7

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    
+    
+    render() {
+
+        const seconds = this.props.initialCount === 0 ? 'Boom!' : `${this.props.initialCount} seconds left before I go boom!`
+
+        return (
+            <div>
+                {seconds}
+            </div>
+        )
+    }
+}
